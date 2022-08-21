35702429f7645cf848ed52ca047d6a04242a0653
Divya6110 <78202049+Divya6110@users.noreply.github.com>
2022-08-21 21:26:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express=require('express')
-var bodyParser=require('body-parser')
-var app=express()
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/bmi.html")
 })
 app.post('/', function(req, res){
-    var h = parseFloat( req.body.height)
-    var w = parseFloat( req.body.weight)
-    var bmi = w /(h*h)
+    const h = parseFloat( req.body.height)
+    const w = parseFloat( req.body.weight)
+    const bmi = w /(h*h)
     bmi= bmi.toFixed()
-    var req_name=req.body.Name
+    const req_name=req.body.Name
     res.send(`<h1>Hello ${req_name} Your BMI Around:${bmi} kg/m^2</h1>`)
     
 })
