edb0244798f5df7b8130548ee7a05c56ff9de361
Divya6110 <78202049+Divya6110@users.noreply.github.com>
2022-08-22 08:56:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get('/',function(req,res){
-    res.sendFile(__dirname+"/bmi.html");
-});
-app.post('/', function(req, res){
-    const h = parseFloat( req.body.height);
-    const w = parseFloat( req.body.weight);
-    var bmi = w /(h*h);
-    bmi= bmi.toFixed();
-    const req_name=req.body.Name;
-    res.send(`<h1>Hello ${req_name}, 
-    Your BMI Around:${bmi} kg/m^2</h1>`);
-    
-    
+    res.sendFile(__dirname+"/bmi.html")
 })
+
+app.post('/', function(req, res){
+    const h = parseFloat( req.body.height)
+    const w = parseFloat( req.body.weight)
+    var bmi = w /(h*h)
+    bmi= bmi.toFixed()
+    const req_name=req.body.Name
+    res.send(`<body style='background-color:rgb(93, 80, 80)'><h1 style='text-align:center;margin-top:5rem'>Hello ${req_name} Your BMI is Around:${bmi} kg/m^2</h1></body>`)
+
+})
+
 app.listen(4000, function(req, res){
-    console.log("Server Running Successfully ...");
-
-
+    console.log("Server Running Successfully ...")
 })
